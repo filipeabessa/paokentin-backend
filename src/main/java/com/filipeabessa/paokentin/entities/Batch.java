adab5f5de355d84beae396fb7d064c2056f49f65
filipegbessa <filipegbessa>
2023-07-01 23:13:29 -0300

    feat(): create getters and setters to Batch attributes

--- src/main/java/com/filipeabessa/paokentin/entities/Batch.java
+++ src/main/java/com/filipeabessa/paokentin/entities/Batch.java
@@ -2,7 +2,30 @@ package com.filipeabessa.paokentin.entities;
 
 public class Batch {
   private int id;
-    private int breadsQuantity;
-    private BreadType breadType;
+  private int breadsQuantity;
+  private BreadType breadType;
 
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getBreadsQuantity() {
+    return breadsQuantity;
+  }
+
+  public void setBreadsQuantity(int breadsQuantity) {
+    this.breadsQuantity = breadsQuantity;
+  }
+
+  public BreadType getBreadType() {
+    return breadType;
+  }
+
+  public void setBreadType(BreadType breadType) {
+    this.breadType = breadType;
+  }
 }
